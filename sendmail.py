ac9e3e763c804448d71b2096d124163c4c592913
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2021-07-25 17:10:55 -0500

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -3,20 +3,20 @@ import yagmail
 yag = yagmail.SMTP("example@gmail.com", "password")
 
 # get daily char message
-emailFile = open("/emailtemplates.txt", "r").readlines()
+emailFile = open("emailtemplates.txt", "r").readlines()
 message = emailFile[0]
 message = message[1:-2]
 
 # overwrite emailtemplates.txt with the daily message removed
-with open("/emailtemplates.txt", "w") as deleteFile:
+with open("emailtemplates.txt", "w") as deleteFile:
     deleteFile.write("")
-with open("/emailtemplates.txt", "a") as rewriteFile:
+with open("emailtemplates.txt", "a") as rewriteFile:
     rewriteFile.write("")
     for eachTemplate in emailFile[1:]:
         rewriteFile.write(eachTemplate)
 
 # email handler
-inputFile = open("/receiverlist.txt", "r").readlines()
+inputFile = open("receiverlist.txt", "r").readlines()
 emailList = []
 for eachEmail in inputFile:
     emailList.append(eachEmail.strip())
